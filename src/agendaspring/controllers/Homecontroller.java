c35ccbae8ed93dfba33b18ea2597a21aecb9bf32
3049255 <3049255@MC266459.ifrn.local>
2018-10-08 07:27:22 -0300

    test vai la

--- src/agendaspring/controllers/Homecontroller.java
+++ src/agendaspring/controllers/Homecontroller.java
@@ -1,6 +1,22 @@
 package agendaspring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class Homecontroller {
 
+	
+	
+	
+	@RequestMapping("/")
+	public String homeseila() {
+		
+		System.out.println("chamou o metodo da home");
+		return "home.jsp";
+		
+	}
+	
+	
+	
 }
